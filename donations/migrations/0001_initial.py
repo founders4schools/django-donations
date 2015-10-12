415753ac0c74e0448cf51180683116c2582a56ee
Andrew Miller <andrew.miller@founders4schools.org.uk>
2015-10-12 22:40:49 +0100

    remove explicit unicode strings

--- donations/migrations/0001_initial.py
+++ donations/migrations/0001_initial.py
@@ -9,25 +9,25 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'Frequency'
-        db.create_table(u'donations_frequency', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table('donations_frequency', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('interval', self.gf('timedelta.fields.TimedeltaField')()),
         ))
-        db.send_create_signal(u'donations', ['Frequency'])
+        db.send_create_signal('donations', ['Frequency'])
 
         # Adding model 'DonationProvider'
-        db.create_table(u'donations_donationprovider', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table('donations_donationprovider', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(default='BlankProvider', max_length=100)),
             ('description', self.gf('django.db.models.fields.CharField')(default='', max_length=255)),
             ('klass', self.gf('django.db.models.fields.CharField')(default='', max_length=255)),
         ))
-        db.send_create_signal(u'donations', ['DonationProvider'])
+        db.send_create_signal('donations', ['DonationProvider'])
 
         # Adding model 'Donation'
-        db.create_table(u'donations_donation', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table('donations_donation', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('amount_currency', self.gf('djmoney.models.fields.CurrencyField')(default='GBP')),
             ('amount', self.gf('djmoney.models.fields.MoneyField')(max_digits=10, decimal_places=2, default_currency='GBP')),
             ('provider', self.gf('django.db.models.fields.related.ForeignKey')(related_name='donations', to=orm['donations.DonationProvider'])),
@@ -44,58 +44,58 @@ class Migration(SchemaMigration):
             ('est_tax_reclaim', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=10, decimal_places=2, blank=True)),
             ('provider_source', self.gf('django.db.models.fields.CharField')(max_length=50, null=True, blank=True)),
         ))
-        db.send_create_signal(u'donations', ['Donation'])
+        db.send_create_signal('donations', ['Donation'])
 
 
     def backwards(self, orm):
         # Deleting model 'Frequency'
-        db.delete_table(u'donations_frequency')
+        db.delete_table('donations_frequency')
 
         # Deleting model 'DonationProvider'
-        db.delete_table(u'donations_donationprovider')
+        db.delete_table('donations_donationprovider')
 
         # Deleting model 'Donation'
-        db.delete_table(u'donations_donation')
+        db.delete_table('donations_donation')
 
 
     models = {
-        u'auth.group': {
+        'auth.group': {
             'Meta': {'object_name': 'Group'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
             'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
         },
-        u'auth.permission': {
-            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
             'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
-        u'auth.user': {
+        'auth.user': {
             'Meta': {'object_name': 'User'},
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
         },
-        u'contenttypes.contenttype': {
+        'contenttypes.contenttype': {
             'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        u'donations.donation': {
+        'donations.donation': {
             'Meta': {'object_name': 'Donation'},
             'amount': ('djmoney.models.fields.MoneyField', [], {'max_digits': '10', 'decimal_places': '2', 'default_currency': "'GBP'"}),
             'amount_currency': ('djmoney.models.fields.CurrencyField', [], {'default': "'GBP'"}),
@@ -104,7 +104,7 @@ class Migration(SchemaMigration):
             'est_tax_reclaim': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '2', 'blank': 'True'}),
             'finished_uri': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
             'frequency': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'donations'", 'to': u"orm['donations.Frequency']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_verified': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'local_amount': ('djmoney.models.fields.MoneyField', [], {'max_digits': '10', 'decimal_places': '2', 'default_currency': "'GBP'"}),
             'local_amount_currency': ('djmoney.models.fields.CurrencyField', [], {'default': "'GBP'"}),
@@ -114,16 +114,16 @@ class Migration(SchemaMigration):
             'provider_source': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
             'status': ('django.db.models.fields.CharField', [], {'default': "'Unverified'", 'max_length': '50'})
         },
-        u'donations.donationprovider': {
+        'donations.donationprovider': {
             'Meta': {'object_name': 'DonationProvider'},
             'description': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'klass': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
             'name': ('django.db.models.fields.CharField', [], {'default': "'BlankProvider'", 'max_length': '100'})
         },
-        u'donations.frequency': {
+        'donations.frequency': {
             'Meta': {'object_name': 'Frequency'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'interval': ('timedelta.fields.TimedeltaField', [], {}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         }
