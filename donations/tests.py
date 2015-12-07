33336410f2ab1c7339f210a0cdba4f889fac84f1
Bruno Alla <bruno.alla@founders4schools.org.uk>
2015-12-07 11:21:25 +0000

    Start writing actual test cases

--- donations/tests.py
+++ donations/tests.py
@@ -1,7 +1,32 @@
 from django.test import TestCase
+from .models import Donation, DonationProvider, Frequency
 
 
-class NoddyTestCase(TestCase):
+class ModelTest(TestCase):
 
-    def test_long_nose(self):
-        self.assertEqual(1+1, 2)
+    def test_frequency(self):
+        Frequency.objects.create(name='single', interval='0 days')
+        self.assertEqual(Frequency.objects.count(), 1)
+
+    def test_provider(self):
+        DonationProvider.objects.create(
+            name='Just Giving',
+            description='Just Giving Provider',
+            klass='just_giving.SimpleDonationProvider',
+        )
+        self.assertEqual(DonationProvider.objects.count(), 1)
+
+
+class DonationTest(TestCase):
+
+    def setUp(self):
+        self.frequency = Frequency.objects.create(name='single', interval='0 days')
+        self.provider = DonationProvider.objects.create(
+            name='Just Giving',
+            description='Just Giving Provider',
+            klass='just_giving.SimpleDonationProvider',
+        )
+
+    def test_create(self):
+        Donation.objects.create(amount=10, provider=self.provider, frequency=self.frequency)
+        self.assertEqual(Donation.objects.count(), 1)
