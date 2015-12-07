940a61ceb93599977ff09720b057d5d116dd6a62
Bruno Alla <bruno.alla@founders4schools.org.uk>
2015-12-07 14:24:02 +0000

    Bump version: 0.2.4 → 0.2.5

--- donations/__init__.py
+++ donations/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
 
 def setup():
