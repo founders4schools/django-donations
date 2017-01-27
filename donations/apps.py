a174dbae36038a68a5f73f2cc45267add3ec0020
Bruno Alla <bruno.alla@founders4schools.org.uk>
2017-01-27 11:11:46 +0000

    Auto-load config into DB again

--- donations/apps.py
+++ donations/apps.py
@@ -5,3 +5,8 @@ from django.apps import AppConfig
 class DonationsConfig(AppConfig):
     name = 'donations'
     verbose_name = "Django Donations"
+
+    def ready(self):
+        from .config_loaders import load_providers, load_frequencies
+        load_providers()
+        load_frequencies()
