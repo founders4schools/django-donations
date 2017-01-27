1af4ed0d2336a4d6f633488a842f670fa0dffea1
Bruno Alla <bruno.alla@founders4schools.org.uk>
2017-01-27 11:56:29 +0000

    Basic tests for views

--- tests/test_views.py
+++ tests/test_views.py
@@ -9,3 +9,11 @@ class DonateViewTest(TestCase):
     def test_get_donation_normal(self):
         response = self.client.get(reverse('testapp:index'))
         self.assertContains(response, "Donate Now")
+
+    def test_get_donation_fixed(self):
+        response = self.client.get(reverse('testapp:fixed'))
+        self.assertContains(response, "Donate Now")
+
+    def test_get_donation_thank_you(self):
+        response = self.client.get(reverse('testapp:thank-you'))
+        self.assertContains(response, "Thank You for your donation")
