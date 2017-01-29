0d79abc81de8659c0c0ce3523d8f2def7b12c595
Bruno Alla <bruno.alla@founders4schools.org.uk>
2017-01-29 09:42:09 +0000

    Setup the admin in tests so it's covered

--- donations/admin.py
+++ donations/admin.py
@@ -6,18 +6,16 @@ from django.contrib import admin
 from donations.models import Donation, Frequency, DonationProvider
 
 
+@admin.register(Donation)
 class DonationAdmin(admin.ModelAdmin):
     list_display = ('amount', 'provider', 'frequency', 'datetime', 'donor', 'is_verified')
 
 
+@admin.register(Frequency)
 class FrequencyAdmin(admin.ModelAdmin):
     list_display = ('name', 'interval')
 
 
+@admin.register(DonationProvider)
 class DonationProviderAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'klass')
-
-
-admin.site.register(Donation, DonationAdmin)
-admin.site.register(Frequency, FrequencyAdmin)
-admin.site.register(DonationProvider, DonationProviderAdmin)
