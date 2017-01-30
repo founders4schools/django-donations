a86d86a1826be8746874915c55e16496a0075aef
Bruno Alla <bruno.alla@founders4schools.org.uk>
2017-01-30 11:51:27 +0000

    Bump version: 0.5.0 → 0.6.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = '0.5.0'
+version = '0.6.0'
 
 readme = codecs.open('README.rst', 'r', 'utf-8').read()
 history = codecs.open('HISTORY.rst', 'r', 'utf-8').read().replace('.. :changelog:', '')
