b76bb842293fe934a2967b6b62099a907a82652d
Bruno Alla <bruno.alla@founders4schools.org.uk>
2018-05-30 15:41:47 +0100

    Django 2.0: Add app_name to urls included with namespace

--- donations/urls.py
+++ donations/urls.py
@@ -4,14 +4,14 @@ from donations.views import DonateAPI, VerifyAPI
 
 app_name = 'donations'
 
-api_urls = [
+api_urls = ([
     url(r'^donate/$', DonateAPI.as_view(), name="donate"),
     url(r'^verify/(?P<pk>[0-9]+)$', VerifyAPI.as_view(), name="verify"),
-]
+], "donations")
 
-donations = [
+donations = ([
     url(r'^api/', include(api_urls, namespace="api")),
-]
+], "donations")
 
 urlpatterns = [
     url(r'^', include(donations, namespace="donations"))
--- tests/urls.py
+++ tests/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import url, include
 from donations.urls import urlpatterns as donations_urls
 from tests.views import SimpleDonateView, FixedDonateView, DonateThankYouView
 
-test_app_urls = [
+test_app_urls = ([
     url(r'^$', SimpleDonateView.as_view(), name='index'),
     url(r'^fixed/$', FixedDonateView.as_view(), name='fixed'),
     url(r'^thank-you/$', DonateThankYouView.as_view(), name='thank-you')
-]
+], "testapp")
 
 urlpatterns = [
     url(r'^', include(donations_urls)),
